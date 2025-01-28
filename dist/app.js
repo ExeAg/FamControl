a52a7f52092a687f59d473ed4db604ef23a03a9c
ExeAg <exe.aguero.exe@gmail.com>
2025-01-28 11:29:53 -0300

    tsc

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = void 0;
+exports.nombre = "Exequiel";
+console.log(exports.nombre);
 //# sourceMappingURL=app.js.map
