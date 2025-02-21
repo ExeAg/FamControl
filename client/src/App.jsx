6046b6bcc2c869da85008854cac787be720e87d0
ExeAg <112787222+ExeAg@users.noreply.github.com>
2025-02-21 13:49:16 -0300

    Front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
+import { ProtectedRoute } from "./routes";
+
+import RegisterPage from "./pages/RegisterPage";
+import { LoginPage } from "./pages/LoginPage";
+import ResumenPage from "./pages/ResumenPage";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
-      <Route path="/loguin" element={<h1>login</h1>} />
-      <Route path="/register" element={<h1>Register</h1>} />
-      <Route path="/resume" element={<h1>resume</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/loguin" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/resumen" element={<ResumenPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
